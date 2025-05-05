ca319c0000c96c54d9cdee3a9afaed407f723403
nyolarraklay <114483893+nyolarraklay@users.noreply.github.com>
2025-05-05 21:04:25 +0200

    started homepage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import { useState } from 'react'
+import { Box, Button, Container, Heading, Input, useToast, useColorModeValue, VStack } from '@chakra-ui/react'
+import { useProductStore } from '../store/product'
 
 const CreatePage = () => {
+  const [newProduct,  setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+
+  });
+
+  const toast = useToast()
+const {createProduct} = useProductStore()
+  const handleAddProduct =  async () => {
+   const {success, message} = await createProduct(newProduct);
+ if(!success){
+  toast({
+    title: "Error",
+    description: message,
+    status: "error",
+    duration: 3000,
+    isClosable: true,
+  })}
+  else {
+    toast({
+      title: "Success",
+      description: message,
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    })
+  }
+  setNewProduct({
+    name: "",
+    price: "",
+    image: "",
+  })
+  }
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>
+      <VStack
+      spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>CreatePage</Heading>
+        <Box
+          as={"form"}
+          borderWidth={1}
+          borderRadius={"lg"}
+          p={8}
+          boxShadow={"lg"}
+          bg={useColorModeValue("white", "gray.800")}
+        >
+          <VStack spacing={4}>
+            <Input placeholder={"Name"} value={newProduct.name} onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}/>
+            <Input placeholder={"Price"} type={"number"} value={newProduct.price} onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}/>
+            <Input placeholder={"Image URL"} value={newProduct.image} onChange={(e) => setNewProduct({...newProduct, image: e.target.value})}/>
+            <Button colorScheme='blue' onClick={handleAddProduct} w={"full"}>Add Product</Button>
+
+          </VStack>
+        </Box>
+      </VStack>
+      </Container>
   )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Container, Text, VStack} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.xl"} py={12}>
+      <VStack spacing={8}>
+<Text fontSize
+      ={"30"} fontWeight={"bold"} bgGradient="linear(to-r, cyan.400, blue.500)" bgClip="text" textAlign={"center"}>
+        Current Products ✈️
+      </Text>
+
+
+
+      <Text fontSize="xl" textAlign={"center"} fontWeight={"bold"} color="gray.500">
+        No products found 😞 {""}
+        <Link to={"/create"}>
+          <Text as="span" color={"blue.400"} _hover={{ textDecoration: "underline" }}>Create a product</Text>
+        </Link> to get started.
+      </Text>
+
+      </VStack>
+      
+      
+      
+    
+      
+      </Container>
   )
 }
 
